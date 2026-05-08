fffafeb435f1219968a51fd667c665594aaba905
Administrator <gangch@aventura.net.cn>
2026-05-08 23:55:38 +0900

    添加多协议
    Signed-off-by: Administrator <gangch@aventura.net.cn>

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+set -e
 
-# 替换配置文件中的 UUID
-sed -i "s/PASTE_YOUR_UUID_HERE/$UUID/g" config.json
+: "${UUID:?UUID is required}"
+: "${TROJAN_PASSWORD:?TROJAN_PASSWORD is required}"
+: "${ARGO_TOKEN:?ARGO_TOKEN is required}"
+
+# 替换 UUID，VLESS 和 VMess 共用同一个 UUID
+sed -i "s/PASTE_UUID_HERE/$UUID/g" config.json
+
+# 替换 Trojan 密码
+sed -i "s/PASTE_TROJAN_PASSWORD_HERE/$TROJAN_PASSWORD/g" config.json
 
 # 后台运行 sing-box
 sing-box run -c config.json &
 
-# 运行 Cloudflare Tunnel
-cloudflared tunnel --no-autoupdate run --token $ARGO_TOKEN
+# 前台运行 Cloudflare Tunnel
